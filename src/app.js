4152f0e131cad24a2dd6059b2312378915e3ae9e
Helder Castrillón <helder_947@hotmail.com>
2019-03-26 20:18:51 -0500

    init project
    this pull inicialize the project in git

---
+++ src/app.js
@@ -0,0 +1,21 @@
+//React Library
+import React from 'react';
+import ReactDOM from 'react-dom';
+//Materials Components
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+//My Components
+import theme from './theme'
+
+function App() {
+  return (
+    <div>
+      <MuiThemeProvider theme={theme}>
+       {"Hola mundo"}
+      </MuiThemeProvider>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
